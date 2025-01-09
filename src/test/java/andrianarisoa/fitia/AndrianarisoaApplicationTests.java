dc79d8ca1063ba3e733b3540234a9b0d477b7506
fitia <fitiandrianarisoa@gmail.com>
2025-01-09 09:44:23 +0100

    init project

---
+++ src/test/java/andrianarisoa/fitia/AndrianarisoaApplicationTests.java
@@ -0,0 +1,13 @@
+package andrianarisoa.fitia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndrianarisoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
