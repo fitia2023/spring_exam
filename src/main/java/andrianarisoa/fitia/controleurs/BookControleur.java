44730074712dbf6e5a64780534ce4eb0140178a5
fitia <fitiandrianarisoa@gmail.com>
2025-01-09 11:09:32 +0100

    Controleur

---
+++ src/main/java/andrianarisoa/fitia/controleurs/BookControleur.java
@@ -0,0 +1,42 @@
+package andrianarisoa.fitia.controleurs;
+
+import andrianarisoa.fitia.dto.BookDto;
+import andrianarisoa.fitia.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/book")
+public class BookControleur {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("all")
+    public List<BookDto> getBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("{id}")
+    public BookDto getBookById(@PathVariable("id") Long id) {
+        return bookService.getBookById(id);
+    }
+
+    @PostMapping("add")
+    public BookDto addBook(@RequestBody BookDto bookDto) {
+        return bookService.addBook(bookDto);
+    }
+
+    @PutMapping("up/{id}")
+    public BookDto updateBook(@PathVariable("id") Long id, @RequestBody BookDto bookDto) {
+        return bookService.updateBook(bookDto);
+    }
+
+    @DeleteMapping("del/{id}")
+    public void deleteBook(@PathVariable("id") Long id) {
+        bookService.deleteBookById(id);
+    }
+
+}
